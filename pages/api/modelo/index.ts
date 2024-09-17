77c5cf9937767db8c71d9c8ef52e8e2195aa4576
Belicio Batista Cardoso <belloinfo@gmail.com>
2024-09-17 15:53:38 -0300

    Create API Modelo

--- pages/api/modelo/index.ts
+++ pages/api/modelo/index.ts
@@ -21,6 +21,25 @@ export default async function handler(
       },
     })
     res.status(200).json(modelo)
+  } else if (req.method === 'PUT') {
+    const { id, nome } = req.body
+    const modelo = await prisma.modelo.update({
+      where: {
+        id,
+      },
+      data: {
+        nome,
+      },
+    })
+    res.status(200).json(modelo)
+  } else if (req.method === 'DELETE') {
+    const { id } = req.body
+    const modelo = await prisma.modelo.delete({
+      where: {
+        id,
+      },
+    })
+    res.status(200).json(modelo)
   } else {
     res.setHeader('Allow', ['GET', 'POST'])
     res.status(405).end(`Method ${req.method} Not Allowed`)
